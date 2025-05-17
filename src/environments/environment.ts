6a56851dc08a66b0411fda243eedf946a5df50b3
christopherMore1 <christopmore28@gmail.com>
2025-05-16 21:24:02 -0500

    Hotech

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   serverBasePath: 'http://localhost:8080/api/v1',
-  prodBasePath: 'https://inncontrol-api.ryzeon.me/api/v1'
+  prodBasePath: 'https://hotech-api.ryzeon.me/api/v1'
 };
