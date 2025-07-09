376998d03e0b6ecc3d9f158b3417042348ec15a3
christopherMore1 <christopmore28@gmail.com>
2025-07-08 23:10:13 -0500

    http

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverBasePath: '20.197.240.203:9101',
-  prodBasePath: '20.197.240.203:9101'
+  serverBasePath: 'http://20.197.240.203:9101',
+  prodBasePath: 'http://20.197.240.203:9101'
 };
